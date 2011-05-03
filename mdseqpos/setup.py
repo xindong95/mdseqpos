fefd72ac815a74fcf675d685e8c7fbfb30be4e77
Tao Liu <vladimir.liu@gmail.com>
2011-05-03 18:32:13 -0400

    1. SeqPos: Remove settings.py which should be customized before user install seqpos. 2. SeqPos: setup.py check if settings.py exists.

--- mdseqpos/setup.py
+++ mdseqpos/setup.py
@@ -25,13 +25,24 @@ def check_pkg_dependencies():
         sys.stderr.write("CRITICAL: numpy 1.3 or greater must be installed\n")
         sys.exit(1)
 
+def check_settings_file():
+    # Do not include lib/settings.py in distribution only
+    # lib/settings.py.sample; so if there is no lib/settings.py, quit
+    # with some information.
+    if os.path.isfile("lib/settings.py"):
+        pass
+    else:
+        std.stderr.write("CRITICAL: Please copy the lib/settings.py.sample to lib/settings.py, and modify the ASSEMBLY_DIR setting!")
+        sys.exit(1)
+
 def main():
     if not float(sys.version[:3])>=2.6:
         sys.stderr.write("CRITICAL: Python version must be greater than or equal to 2.4! python 2.6.1 is recommended!\n")
         sys.exit(1)
     check_pkg_dependencies()
+    check_settings_file()
     setup(name="mdseqpos",
-          version="0.590",
+          version="0.591",
           description="Motif finding tools",
           author='Ying Lei, Len Taing, et al',
           author_email='lentaing@jimmy.harvard.edu',
